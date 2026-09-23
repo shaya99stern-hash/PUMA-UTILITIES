dfce4f5f996604b1408f3b493684bbac9c0b7f22
shaya99stern-hash <shaya99stern@gmail.com>
2026-09-23 11:10:00 -0400

    feat: load shared Puma application shell styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,7 @@ import './ios-native.css';
 import './puma-responsive-v6.css';
 import './puma-minimal-settings.css';
 import './puma-polish-v12.css';
+import './puma-app-shell.css';
 
 const APPLE_ICON = '/apple-touch-icon?v=20260923-2';
 const PWA_ICON_192 = '/pwa-icon-192?v=20260923-2';
